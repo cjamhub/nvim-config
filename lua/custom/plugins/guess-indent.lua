283d45000f80dc11f0a7f49313ac9fd6c9bde536
Jam Chen <jam@Jams-MacBook-Air-5.local>
2025-05-14 16:50:29 +0100

    add gitsigns & lint-for-markdown

--- lua/custom/plugins/guess-indent.lua
+++ lua/custom/plugins/guess-indent.lua
@@ -9,4 +9,11 @@ return {
       }
     end,
   },
+  { -- Add indentation guides even on blank lines
+    'lukas-reineke/indent-blankline.nvim',
+    -- Enable `lukas-reineke/indent-blankline.nvim`
+    -- See `:help ibl`
+    main = 'ibl',
+    opts = {},
+  },
 }
