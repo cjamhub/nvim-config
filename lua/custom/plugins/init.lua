13e2e09df2299b99ebedd1569b31861d93545ac6
Jam Chen <jam@Jams-MacBook-Air-5.local>
2025-05-15 14:46:31 +0100

    disable auto fold

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -3,7 +3,11 @@
 --
 -- See the kickstart.nvim README for more information
 return {
+  -- keymaps
   vim.keymap.set('n', '<leader>e', ':e%:h<CR>'),
   vim.keymap.set('n', '<C-j>', ':tabnext<CR>'),
   vim.keymap.set('n', '<C-k>', ':tabprev<CR>'),
+
+  -- set fuzzy search current buffer
+  vim.keymap.set('n', '<C-s>', ':Telescope current_buffer_fuzzy_find<CR>'),
 }
--- lua/custom/plugins/nvim-treesitter.lua
+++ lua/custom/plugins/nvim-treesitter.lua
@@ -4,6 +4,11 @@ return {
     'nvim-treesitter/nvim-treesitter',
     opts = function(_, opts)
       vim.list_extend(opts.ensure_installed, { 'python', 'go', 'rust' })
+      opts.fold = {
+        enable = true,
+        method = 'expr',
+        expr = 'nvim_treesitter#foldexpr()',
+      }
       return opts
     end,
   },
